f95f6a7c3d7eb8acdb5223bd2f0b156f3094b065
Muhamad Barhan <muhamadbarhan02@gmail.com>
2025-04-17 12:47:40 +0530

    Github-deploy

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,7 +5,7 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
     fingerprint: {
-      prepend: 'https://<your-username>.github.io/<your-repo-name>/'
+      prepend: 'https://MuhamadBarhan.github.io/Ember-Task/'
     }
   });
 
